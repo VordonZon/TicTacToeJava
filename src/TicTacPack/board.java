a14b456dde830abbf17dab0701a06d4974f7bf69
VordonZon <henscheidcarl@gmail.com>
2024-12-16 22:46:14 -0700

    Push TicTac

---
+++ src/TicTacPack/board.java
@@ -0,0 +1,18 @@
+package TicTacPack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface board {
+	private board;
+}
+
+private Map<Integer,Character> makeBoard() {
+	private Map<Integer,Character> board = new HashMap<Integer,Character>()
+			
+	for(int i = 1 ; i <= 9; i++) {
+		board.put(i, null);
+	}
+	return board;
+}
+
